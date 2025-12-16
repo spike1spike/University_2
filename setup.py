f2443a8ad5ac39e6506b7b1f1c858f7dca701be5
spike1spike <43131663+spike1spike@users.noreply.github.com>
2025-12-16 17:05:24 +0300

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup, Extension 
-
-setup(
-    name='minList',               # Имя пакета
-    version='1.0',                 # Версия пакета
-    ext_modules=[Extension('minList', ['Source.cpp'])]  # Указание расширения на C/C++, которое будет собрано из файла Source.cpp
-)
